632412305dbc07b762f8ce84ff149dda5d09b8f8
Ousmane Diallo <ousmanediallo@Ousmanes-MBP.lan>
2023-05-02 09:54:12 -0400

    in progress

--- src/main/java/com/learntocode/Main.java
+++ src/main/java/com/learntocode/Main.java
@@ -1,9 +1,17 @@
 package com.learntocode;
 
 import java.io.*;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 import java.util.Scanner;
 
 public class Main {
+    //private static ArrayList<Transaction> transactions = new ArrayList<Transaction>();
+    private static final String FILE_NAME = "transactions.csv";
+    private static final String DATE_FORMAT = "yyyy-MM-dd";
+    private static final String TIME_FORMAT = "HH:mm:ss";
+    private static final DateTimeFormatter DATE_FORMATTER = DateTimeFormatter.ofPattern(DATE_FORMAT);
+    private static final DateTimeFormatter TIME_FORMATTER = DateTimeFormatter.ofPattern(TIME_FORMAT);
 
 
     public static void main(String[] args) throws IOException {
@@ -18,15 +26,15 @@ public class Main {
             System.out.println("X) Exit");
             System.out.print("Enter your choice: ");
 
-            choice = scanner.nextLine();
+            choice = scanner.nextLine().trim();
 
-            switch (choice) {
+            switch (choice.toUpperCase()) {
                 case "D":
                     AddDeposit();
                     break;
 
                 case "P":
-                    MakePayment();
+                    MakePayment(true);
                     break;
 
                 case "L":
@@ -75,7 +83,7 @@ public class Main {
         //date|time|description|vendor|amount
         FileWriter writer = new FileWriter("transactions.csv");
         writer.write(date+"|"+time+"|"+description+"|"+vendor+"|$"+amount);
-        //System.out.println(deposit);
+
 
         writer.close();
 
@@ -83,7 +91,7 @@ public class Main {
 
     // P) Make Payment (Debit) - prompt user for the debit information and save it to the csv file
 
-    private static void MakePayment() throws IOException {
+    private static void MakePayment(boolean append) {
         Scanner scanner = new Scanner(System.in);
         System.out.println("Enter your debit information");
         System.out.println("Enter date: ");
@@ -102,26 +110,28 @@ public class Main {
         double amount = scanner.nextDouble();
 
 
-
-        FileWriter writer = new FileWriter("transactions.csv");
-        writer.write(date+"|"+time+"|"+description+"|"+vendor+"|"+"$-"+amount);
-
-
-        writer.close();
-
-    }
-    private static void display() throws IOException {
-
-        FileReader fileReader = new FileReader("transactions.csv");
-        BufferedReader bufReader = new BufferedReader(fileReader);
-        String input;
-
-        while((input = bufReader.readLine()) != null) {
-            System.out.println(input);
-
-            bufReader.close();
+        FileWriter writer = null;
+        try {
+            writer = new FileWriter("transactions.csv");
+        } catch (IOException e) {
+            throw new RuntimeException(e);
         }
+        try {
+            writer.write(date+"|"+time+"|"+description+"|"+vendor+"|"+"$-"+amount);
+            writer.flush();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+
+        try {
+            writer.close();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
     }
+
 
     private static void Ledger() throws IOException {
         // display the ledger screen
@@ -135,10 +145,10 @@ public class Main {
         System.out.println("R) Reports");
         System.out.println("H) Home");
         System.out.println("Enter your option: ");
-        choice = scanner.nextLine();
+        choice = scanner.nextLine().trim();
 
 
-        switch (choice) {
+        switch (choice.toUpperCase()) {
             case "A":
                 display();
                 break;
@@ -158,7 +168,93 @@ public class Main {
     }
 
     }
+    private static void display() throws IOException {
+
+        FileReader fileReader = new FileReader("transactions.csv");
+        BufferedReader bufReader = new BufferedReader(fileReader);
+        String input;
+
+        while((input = bufReader.readLine()) != null) {
+            System.out.println(input);
+
+            //bufReader.close();
+        }
+    }
+    private static void displayDeposits() {
+        // This method should display a table of all deposits in the `transactions` ArrayList.
+        // The table should have columns for date, time, vendor, and amount.
+        // The total amount of all deposits should be displayed at the bottom of the table.
+    }
+
+    private static void displayPayments() {
+        // This method should display a table of all payments in the `transactions` ArrayList.
+        // The table should have columns for date, time, vendor, and amount.
+        // The total amount of all payments should be displayed at the bottom of the table.
+    }
+
+    private static void reportsMenu(Scanner scanner) {
+        boolean running = true;
+        while (running) {
+            System.out.println("Reports");
+            System.out.println("Choose an option:");
+            System.out.println("1) Month To Date");
+            System.out.println("2) Previous Month");
+            System.out.println("3) Year To Date");
+            System.out.println("4) Previous Year");
+            System.out.println("5) Search by Vendor");
+            System.out.println("0) Back");
+
+            String input = scanner.nextLine().trim();
+
+            switch (input) {
+                case "1":
+                    // Generate a report for all transactions within the current month,
+                    // including the date, vendor, and amount for each transaction.
+                    // The report should include a total of all transaction amounts for the month.
+                case "2":
+                    // Generate a report for all transactions within the previous month,
+                    // including the date, vendor, and amount for each transaction.
+                    // The report should include a total of all transaction amounts for the month.
+                case "3":
+                    // Generate a report for all transactions within the current year,
+                    // including the date, vendor, and amount for each transaction.
+                    // The report should include a total of all transaction amounts for the year.
+
+                case "4":
+                    // Generate a report for all transactions within the previous year,
+                    // including the date, vendor, and amount for each transaction.
+                    // The report should include a total of all transaction amounts for the year.
+                case "5":
+                    // Prompt the user to enter a vendor name, then generate a report for all transactions
+                    // with that vendor, including the date, vendor, and amount for each transaction.
+                    // The report should include a total of all transaction amounts for the vendor.
+                case "0":
+                    running = false;
+                default:
+                    System.out.println("Invalid option");
+                    break;
+            }
+        }
+    }
+
+
+    private static void filterTransactionsByDate(LocalDate startDate, LocalDate endDate) {
+        // This method filters the transactions by date and prints a report to the console.
+        // It takes two parameters: startDate and endDate, which represent the range of dates to filter by.
+        // The method loops through the transactions list and checks each transaction's date against the date range.
+        // Transactions that fall within the date range are printed to the console.
+        // If no transactions fall within the date range, the method prints a message indicating that there are no results.
+    }
+
+    private static void filterTransactionsByVendor(String vendor) {
+        // This method filters the transactions by vendor and prints a report to the console.
+        // It takes one parameter: vendor, which represents the name of the vendor to filter by.
+        // The method loops through the transactions list and checks each transaction's vendor name against the specified vendor name.
+        // Transactions with a matching vendor name are printed to the console.
+        // If no transactions match the specified vendor name, the method prints a message indicating that there are no results.
+    }
 }
+
 
 
 
